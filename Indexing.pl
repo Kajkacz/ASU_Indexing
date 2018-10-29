9f16f90d83af16f1f6b399b1c2a1decded47f0d0
Kajetan Kaczmarek <Kajkaz@localhost.localdomain>
2018-10-29 23:03:57 +0100

    Work in progres

--- Indexing.pl
+++ Indexing.pl
@@ -7,21 +7,19 @@ use warnings;
 use strict;
 
 my $Mw = MainWindow->new;
-$Mw->geometry("400x200");
+$Mw->geometry("500x300");
 $Mw->title("Indexing");
 
 $Mw->Label(-text => 'Indexer')->pack();
 
 $Mw->Button(-text=>"Close", -command =>sub{exit})->pack();
-$Mw->Button(-text=>"Execute Exact", -command =>sub{&readFromFile})->pack();
-$Mw->Button(-text=>"Execute Regular Expression", -command =>sub{&readFromFile})->pack();
+$Mw->Button(-text=>"Search Exact", -command =>sub{&readFromFile})->pack();
+$Mw->Button(-text=>"Search Regular Expression", -command =>sub{&readFromFile})->pack();
+$Mw->Button(-text=>"Index", -command =>sub{&Index})->pack();
 
 my $filePathEntry = $Mw->Entry(-text=>"Test.txt" )->pack(); #/home/Kajkaz/Dokumenty/ASU_Projekt/
 my $phraseToSearch = $Mw->Entry(-text=>"Phrase to find" )->pack();
 
-dbmopen(%DATA, "IndexDB", 0644)
-  or die "Cannot create IndexDataBase: $!";
- 
 MainLoop;
 
 sub readFromFile{
@@ -36,4 +34,21 @@ while(my $row = <$File>){
 	print "$row\n";
 	
 }
+}
+
+sub Index{
+my $filePath = $filePathEntry->get();
+print("Path of file to open : " ,$filePath, "\n");
+
+open(my $File, '<:encoding(UTF-8)',$filePath)
+	or die"Could not open file '$filePath' $!";
+	
+my %words;
+my $i = 1;
+local $/ = ' ';
+
+while(my $word = <$File>){
+	#$words($word) = $i++;
+	print "$word\n";	
+}	
 }
